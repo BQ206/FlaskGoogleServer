c6bef1e55566309a9d11726e586df765170bdd02
BQ206 <47434995+BQ206@users.noreply.github.com>
2023-04-07 16:26:33 +0100

    Update University.py

--- University.py
+++ University.py
@@ -1,6 +1,6 @@
 import os 
 
-from google.cloud.sql.connector import Connector
+from google.cloud.sql.connector import connector
 import mysql.connector
 import json
 import sqlalchemy
@@ -30,7 +30,7 @@ with open("sample.json", "w") as outfile:
     outfile.write(json_object)
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./sample.json"
-connector = Connector()
+
 
 def ListResearchAccounts():
     conn = connector.connect(
